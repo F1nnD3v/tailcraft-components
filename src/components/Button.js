9d05a59ccc0e7096c60b5060b03713536dfe4f18
F1nn <67049621+F1nnD3v@users.noreply.github.com>
2023-08-21 23:34:51 +0100

    #5

---
+++ src/components/Button.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Loading from "./Loading";
+
+const Button = ({
+  variant = "primary",
+  size = "medium",
+  loading = false,
+  children,
+  text = "Edit me!",
+  ...props
+}) => {
+  const baseClasses =
+    "rounded-md shadow-sm font-medium focus:outline-none focus:ring-2 focus:ring-offset-2";
+
+  const variantClasses = {
+    primary: "text-white bg-blue-600 hover:bg-blue-700",
+    secondary: "text-blue-600 bg-white hover:bg-gray-100",
+  };
+
+  const sizeClasses = {
+    small: "text-sm px-3 py-1",
+    medium: "text-md px-4 py-2",
+    large: "text-lg px-5 py-3",
+  };
+
+  const loadingClasses = loading ? "opacity-50 cursor-not-allowed" : "";
+
+  const classes = `${baseClasses} ${variantClasses[variant]} ${sizeClasses[size]} ${loadingClasses}`;
+
+  return (
+    <button className={classes} loading={loading} {...props}>
+      {loading ? <Loading /> : children} {text}
+    </button>
+  );
+};
+
+export default Button;
