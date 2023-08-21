7da5a69c496488c76e7f388efcd990c9f46d002f
F1nn <67049621+F1nnD3v@users.noreply.github.com>
2023-08-21 23:38:32 +0100

    #4

--- src/components/Breadcrumb.js
+++ src/components/Breadcrumb.js
@@ -1,8 +1,21 @@
-const Breadcrumb = () => {
+import React from 'react';
+
+const Breadcrumb = ({ links = [] }) => {
   return (
-    <div>
-      <div className=""></div>
-    </div>
+    <nav className="text-sm text-gray-600">
+      <ul className="flex">
+        {links.map((link, index) => (
+          <li key={link.path}>
+            <a href={link.path} className="text-blue-600 hover:underline">
+              {link.label}
+            </a>
+            {index < links.length - 1 && (
+              <span className="mx-2">/</span>
+            )}
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
 
