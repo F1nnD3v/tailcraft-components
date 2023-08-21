712c9fdd45b9cb6050f1c35279c2e9d38281ff49
F1nn <67049621+F1nnD3v@users.noreply.github.com>
2023-08-22 00:32:33 +0100

    finished v1 of welcome page

--- src/Pages/Welcome.js
+++ src/Pages/Welcome.js
@@ -1,18 +1,30 @@
-import Button from "../components/Button"
+import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
 
 const Welcome = () => {
-    return (
-        <>
-            <div className="text-center text-2xl font-bold">
-                Welcome To <p>Tailcraft Components</p>
-            </div>
-            <div>
-                <Button>
-                    asdasd
-                </Button>
-            </div>
-        </>
-    )
-}
+  const navigate = useNavigate();
 
-export default Welcome
+  const gotoAllComponentsPage = () => {
+    navigate("/all")
+  };
+
+  const gotoDemoPage = () => {
+    navigate("/demo")
+  }
+
+  return (
+    <>
+      <div className="text-center text-2xl font-bold">
+        Welcome To <p>Tailcraft Components</p>
+      </div>
+      <div className="flex justify-center items-center mt-10 flex-col gap-2">
+        <Button size="medium" onClick={() => gotoAllComponentsPage()}>
+          See all components
+        </Button>
+        <Button size="medium" onClick={() => gotoDemoPage()}>Demo page</Button>
+      </div>
+    </>
+  );
+};
+
+export default Welcome;
