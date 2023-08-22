d516e1c5283cbe7148a77795832ff57045c5cad2
F1nn <67049621+F1nnD3v@users.noreply.github.com>
2023-08-22 07:47:31 +0000

    Made some changes to Switch #25

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,2 +1,18 @@
-const Switch = () => {return(<></>)}
+import { useState } from "react"
+
+const Switch = ({textEnabled = "I'm enabled", textDisabled = "I'm disabled", enabled = true, primaryColor = "blue", secondaryColor = "white"}) => {
+    
+    const [isEnabled, setIsEnabled] = useState(enabled);
+
+    const onChange = () => {
+        
+    }
+    
+    return (<div className={`flex justify-around bg-${primaryColor}-500 w-32 text-${secondaryColor} text-center rounded-full p-1 hover:cursor-pointer`} onClick={() => onChange()}>
+            <div className={`bg-${secondaryColor} w-5 h-5 rounded-full my-auto`}></div>
+                {
+                    isEnabled ? textEnabled : textDisabled
+                }
+            </div>)
+}
 export default Switch
