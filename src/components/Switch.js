348ab0980bc4ef8bb0f3819ca5212b35067f7165
F1nn <67049621+F1nnD3v@users.noreply.github.com>
2023-08-22 20:46:01 +0100

    #25 completed but not dynamic

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,17 +1,32 @@
-import { useState } from "react"
+import { useState } from "react";
 
-const Switch = ({textEnabled = "I'm enabled", textDisabled = "I'm disabled", enabled = true, primaryColor = "blue", secondaryColor = "white"}) => {
-    
-    const [isEnabled, setIsEnabled] = useState(enabled);
+const Switch = ({
+  textEnabled = "I'm enabled",
+  textDisabled = "I'm disabled",
+  enabled = false,
+  primaryColor = "blue",
+  secondaryColor = "white",
+}) => {
+  const [isEnabled, setIsEnabled] = useState(enabled);
 
-    const onChange = () => {
-        
-    }
-    
-    return (<div className={`flex justify-around bg-${primaryColor}-500 w-32 text-${secondaryColor} text-center rounded-full p-1 hover:cursor-pointer`} onClick={() => onChange()}>
-            <div className={`bg-${secondaryColor} w-5 h-5 rounded-full my-auto`}></div>
-                {
-                    isEnabled ? textEnabled : textDisabled
-                }
-            </div>)
-}
+  const primaryClass = primaryColor === "blue" ? "bg-blue-500" : "bg-custom-primary";
+  const secondaryClass = secondaryColor === "white" ? "bg-white" : "bg-custom-secondary";
+
+  const onChange = () => {
+    setIsEnabled(!isEnabled);
+  };
+
+  return (
+    <div
+      className={`relative ${primaryClass} h-7 w-36 text-${secondaryColor} text-center rounded-full p-1 cursor-pointer transition-all duration-300`}
+      onClick={onChange}
+    >
+      <div
+        className={`absolute left-1 top-1 ${secondaryClass} w-5 h-5 rounded-full my-auto transition-all duration-300 transform ${isEnabled ? "translate-x-28" : ""}`}
+      ></div>
+      <div className="flex-1 text-center absolute left-0 right-0 top-0 bottom-0 flex items-center justify-center">{isEnabled ? textEnabled : textDisabled}</div>
+    </div>
+  );
+};
+
+export default Switch;
