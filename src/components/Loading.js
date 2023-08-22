f4c965332061aab625f89d084cc6cdda35a86e5d
F1nn <67049621+F1nnD3v@users.noreply.github.com>
2023-08-22 01:07:30 +0100

    Added new features to Loading
    Enhanced Loading Component with Versatile Features
    
    - Added support for multiple loading animation styles, including spinner, pulsing dots, and bars.
    - Introduced an option to display custom loading text or messages.
    - Implemented different color options for the loading indicator.
    - Added a size property to control the loading indicator's dimensions.
    - Created an overlay mode to cover specific areas or the entire page during loading.
    
    These enhancements provide greater flexibility and customization for the Loading component, allowing developers to adapt it to various use cases and design preferences.

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,8 +1,43 @@
-const Loading = ({ text = "Default loading text", ...props }) => {
+const Loading = ({
+  type = "spinner", // "spinner", "dots", "bars"
+  text = "Loading...",
+  color = "black",
+  size = "medium", // "small", "medium", "large"
+  overlay = false,
+  ...props
+}) => {
+  // Style classes for different types of animations
+  const animationClasses = {
+    spinner: "animate-spin border-2 border-t-transparent rounded-full",
+    dots: "animate-pulse",
+    bars: "animate-bounce",
+  };
+
+  // Style classes for different sizes
+  const sizeClasses = {
+    small: "w-4 h-4",
+    medium: "w-5 h-5",
+    large: "w-6 h-6",
+  };
+
+  // Overlay styles
+  const overlayStyles = overlay
+    ? "fixed inset-0 flex items-center justify-center bg-opacity-50 bg-white z-50"
+    : "";
+
+  // Combined classes
+  const classes = `${animationClasses[type]} ${sizeClasses[size]} border-${color} ${overlayStyles}`;
+
   return (
-    <div>
-      <div className="animate-spin w-5 h-5 border-2 border-black rounded-full border-t-transparent m-auto"></div>
-      <p className="text-center mt-2">{text}</p>
+    <div
+      className={
+        overlay
+          ? "fixed inset-0 flex items-center justify-center bg-opacity-50 bg-white z-50"
+          : ""
+      }
+    >
+      <div className={classes}></div>
+      {text && <p className="text-center mt-2">{text}</p>}
     </div>
   );
 };
